0318367811e2d6e12f186fc067038aa553fb99c4
Eduardo silva <78171718+Duh130@users.noreply.github.com>
2025-09-25 09:16:48 -0300

    correçao vercel

--- mysyte/urls.py
+++ mysyte/urls.py
@@ -1,8 +1,9 @@
-# mysyte/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from blog.views import home  # sua view da home
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),  # todas as rotas do blog
+    path('admin/', admin.site.urls),     # rota do admin
+    path('blog/', include('blog.urls')), # todas as URLs do blog
+    path('', home, name='home'),         # rota raiz ("/")
 ]
