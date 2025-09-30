4871911336e11b2bee4a1b04088d7a6a2d7389b3
Eduardo silva <78171718+Duh130@users.noreply.github.com>
2025-09-30 07:10:58 -0300

    Remove arquivos temporários do repositório

--- teste_arquivo.py
+++
@@ -1,8 +0,0 @@
-try:
-    with open("teste.txt", "r", encoding="utf-16") as f:
-        conteudo = f.read()
-    print("Arquivo lido com sucesso!")
-    print("Primeiras linhas do arquivo:")
-    print(conteudo[:200])
-except Exception as e:
-    print("Erro ao ler o arquivo:", e)
